7f0fdc235035fbe2dba98e9c20ce88474dfd2847
jakubr22 <jakub.rozga@gmail.com>
2016-06-10 15:55:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kuba/activitytracker/UserInfoActivity.java
@@ -0,0 +1,48 @@
+package com.example.kuba.activitytracker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+        EditText name = (EditText)findViewById(R.id.editText);
+        sharedPreferences = getSharedPreferences("com.example.artur.aehr", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        name.setText(sharedPreferences.getString("name", "userName"));
+    }
+
+    public void zapiszDane(View view) {
+        EditText name = (EditText)findViewById(R.id.editText);
+        String userName = name.getText().toString();
+        EditText age = (EditText)findViewById(R.id.editText2);
+        String userAge = age.getText().toString();
+        EditText weight = (EditText)findViewById(R.id.editText3);
+        String userWeight = weight.getText().toString();
+        String userSex;
+        RadioButton plecZenska = (RadioButton)findViewById(R.id.radioButton);
+        if(plecZenska.isChecked()) userSex = "woman";
+        else userSex = "man";
+
+
+        editor.putString("name", userName);
+        editor.commit();
+        editor.putString("age", userAge);
+        editor.commit();
+        editor.putString("weight", userWeight);
+        editor.commit();
+        editor.putString("sex", userSex);
+        editor.commit();
+    }
+}
---
+++ app/src/main/java/com/example/kuba/activitytracker/core/Point.java
@@ -0,0 +1,53 @@
+package com.example.kuba.activitytracker.core;
+
+
+import android.location.Location;
+
+import java.util.Date;
+/**
+ * przechowuje lokacje oraz czas
+ *
+ */
+public class Point {
+    //
+    private double longitude;
+    private double latitude;
+    private double altitude;
+    private float speed;
+    Date date;
+    static private int nr=0;
+
+
+
+    public Point(Location loc) {
+        System.out.println("dodano punkt "+nr);
+        nr++;
+        longitude = loc.getLongitude(); //długość
+        latitude = loc.getLatitude();   //szerokość
+        altitude = loc.getAltitude();   //wysokość
+        this.date = new Date();
+        speed = loc.getSpeed();
+
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+}
